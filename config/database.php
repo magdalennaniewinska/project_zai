fe3b0301b1bfe6b2544fe5743e7dd3b3eba04572
magdalenan <magdalenan@qualtrics.com>
2021-11-08 19:48:02 +0100

    config database

--- config/database.php
+++ config/database.php
@@ -2,8 +2,16 @@
 
 use Illuminate\Support\Str;
 
+$url = parse_url(getenv("CLEARDB_DATABASE_URL"));
+
+$host = $url["host"];
+$username = $url["user"];
+$password = $url["pass"];
+$database = substr($url["path"], 1);
+
 return [
 
+    
     /*
     |--------------------------------------------------------------------------
     | Default Database Connection Name
@@ -15,7 +23,7 @@ return [
     |
     */
 
-    'default' => env('DB_CONNECTION', 'mysql'),
+    'default' => env('DB_CONNECTION', 'your_heroku_mysql_connection'),
 
     /*
     |--------------------------------------------------------------------------
@@ -90,6 +98,17 @@ return [
             'prefix' => '',
             'prefix_indexes' => true,
         ],
+
+        'your_heroku_mysql_connection' => array(
+            'driver' => 'mysql',
+            'host' => $host,
+            'database' => $database,
+            'username' => $username,
+            'password' => $password,
+            'charset' => 'utf8',
+            'collation' => 'utf8_unicode_ci',
+            'prefix' => '',
+        ),
 
     ],
 
